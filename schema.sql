fedfe1dd24766b9c80c828070bc59cd07ea80409
onesis <Onesis.olea@gmail.com>
2021-10-07 19:26:50 -0400

    Modify animals table:
    Remove column species
    
    Add column species_id which is a foreign key referencing species table
    
    Add column owner_id which is a foreign key referencing the owners table

--- schema.sql
+++ schema.sql
@@ -26,3 +26,12 @@ CREATE TABLE species (
     name VARCHAR(50),
     PRIMARY KEY(id)
 );
+
+/* Remove column species */
+ALTER TABLE animals DROP COLUMN species;
+/* Add column species_id which is a foreign key referencing species table */
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
+/* Add column owner_id which is a foreign key referencing the owners table */
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ALTER TABLE animals ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
